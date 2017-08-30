2bc6d154e941ddb3d7aa2465ab51ce03fcab9718
Paul Bailey <pbailey@dx4860>
2017-08-30 14:37:58 -0700

    compile(): Simplify code.
    Pull a deeply nested statement into a helper function.
    Use buffer_t infrastructure for expbuf[].

--- src/code.c
+++ src/code.c
@@ -1,6 +1,7 @@
 #include "ed.h"
 #include <string.h>
 #include <assert.h>
+#include <stdlib.h>
 #include <ctype.h>
 
 enum {
@@ -25,7 +26,8 @@ static struct bralist_t bralist[NBRA];
 static int nbra;
 
 static int circfl;
-static char expbuf[ESIZE + 4];
+static char expbuf_arr[ESIZE + 4];
+static struct buffer_t expbuf = BUFFER_INITIAL(expbuf_arr, sizeof(expbuf_arr));
 static char *locs;
 
 /* Length of a backref in bralist, by index */
@@ -208,7 +210,7 @@ execute(int *addr, int *zaddr)
                 p1 = tempf_to_line(*addr, &linebuf);
                 locs = NULL;
         }
-        p2 = expbuf;
+        p2 = expbuf.base;
         if (circfl) {
                 loc1 = p1;
                 return advance(p1, p2);
@@ -234,24 +236,76 @@ execute(int *addr, int *zaddr)
                         loc1 = p1;
                         return 1;
                 }
-        } while (*p1++);
+        } while (*p1++ != '\0');
         return 0;
+}
+
+int
+lastexp(void)
+{
+        return *(buffer_ptr(&expbuf) - 1);
+}
+
+/*
+ * Helper to compile().
+ * *pps points to the line after '['.
+ */
+static int
+compile_bracket(char **pps, int eof)
+{
+        int cclcnt = 1;
+        int c;
+        char *s = *pps;
+
+        buffer_putc(&expbuf, CCL);
+        buffer_putc(&expbuf, '\0');
+        if ((c = *s++) == '^') {
+                c = *s++;
+                *(buffer_ptr(&expbuf) - 2) = NCCL;
+        }
+        do {
+                if (c == '\n' || c == '\0')
+                        return -1;
+                if (c == '-' && lastexp() != '\0') {
+                        int c2;
+                        if ((c = *s++) == ']') {
+                                buffer_putc(&expbuf, '-');
+                                cclcnt++;
+                                break;
+                        } else if (c == '\0') {
+                                return -1;
+                        }
+                        while ((c2 = lastexp()) < c) {
+                                if (expbuf.count >= expbuf.size)
+                                        return -1;
+                                buffer_putc(&expbuf, c2 + 1);
+                                cclcnt++;
+                        }
+                }
+                if (expbuf.count >= expbuf.size)
+                        return -1;
+                buffer_putc(&expbuf, c);
+                cclcnt++;
+        } while ((c = *s++) != ']');
+        *pps = s;
+        return cclcnt;
 }
 
 void
 compile(int aeof)
 {
-        int eof, c, c2;
-        char *ep;
+        int eof, c;
         char *lastep;
         char bracket[NBRA], *bracketp;
-        int cclcnt;
+        char *tbuf;
+        char *s;
+        size_t len;
 
-        ep = expbuf;
+        buffer_reset(&expbuf);
         eof = aeof;
         bracketp = bracket;
         if ((c = getchr()) == eof) {
-                if (*ep == '\0')
+                if (expbuf.base[0] == '\0')
                         qerror();
                 return;
         }
@@ -266,50 +320,58 @@ compile(int aeof)
         ungetchr(c);
         lastep = NULL;
 
+        s = tbuf = ttgetdelim(eof);
+        if (s == NULL
+            || (len = strlen(s) + 1) >= expbuf.size
+            || s[len - 1] != aeof) {
+                goto cerror;
+        }
+
         for (;;) {
-                if (ep >= &expbuf[ESIZE])
+                c = *s++;
+                if (c == '\0')
                         goto cerror;
-                c = getchr();
                 if (c == eof) {
                         if (bracketp != bracket)
                                 goto cerror;
-                        *ep++ = C_EOF;
+                        buffer_putc(&expbuf, C_EOF);
+                        free(tbuf);
                         return;
                 }
                 if (c != '*')
-                        lastep = ep;
+                        lastep = buffer_ptr(&expbuf);
 
                 switch (c) {
 
                 case '\\':
-                        if ((c = getchr()) == '(') {
+                        if ((c = *s++) == '(') {
                                 if (nbra >= NBRA)
                                         goto cerror;
                                 *bracketp++ = nbra;
-                                *ep++ = CBRA;
-                                *ep++ = nbra++;
+                                buffer_putc(&expbuf, CBRA);
+                                buffer_putc(&expbuf, nbra++);
                                 continue;
                         }
                         if (c == ')') {
                                 if (bracketp <= bracket)
                                         goto cerror;
-                                *ep++ = CKET;
-                                *ep++ = *--bracketp;
+                                buffer_putc(&expbuf, CKET);
+                                buffer_putc(&expbuf, *--bracketp);
                                 continue;
                         }
                         if (c >= '1' && c < '1' + NBRA) {
-                                *ep++ = CBACK;
-                                *ep++ = c - '1';
+                                buffer_putc(&expbuf, CBACK);
+                                buffer_putc(&expbuf, c - '1');
                                 continue;
                         }
-                        *ep++ = CCHR;
+                        buffer_putc(&expbuf, CCHR);
                         if (c == '\n')
                                 goto cerror;
-                        *ep++ = c;
+                        buffer_putc(&expbuf, c);
                         continue;
 
                 case '.':
-                        *ep++ = CDOT;
+                        buffer_putc(&expbuf, CDOT);
                         continue;
 
                 case '\n':
@@ -322,55 +384,30 @@ compile(int aeof)
                         continue;
 
                 case '$':
-                        c2 = getchr();
-                        ungetchr(c2);
-                        if (c2 != eof) {
+                    {
+                        int c2 = *s++;
+                        --s;
+                        if (c2 != eof)
                                 goto defchar;
-                        }
-                        *ep++ = CDOL;
+                        buffer_putc(&expbuf, CDOL);
                         continue;
+                    }
 
                 case '[':
-                        *ep++ = CCL;
-                        *ep++ = '\0';
-                        cclcnt = 1;
-                        if ((c = getchr()) == '^') {
-                                c = getchr();
-                                ep[-2] = NCCL;
-                        }
-                        do {
-                                if (c == '\n')
-                                        goto cerror;
-                                if (c == '-' && ep[-1] != '\0') {
-                                        if ((c = getchr()) == ']') {
-                                                *ep++ = '-';
-                                                cclcnt++;
-                                                break;
-                                        }
-                                        while (ep[-1] < c) {
-                                                *ep = ep[-1] + 1;
-                                                ep++;
-                                                cclcnt++;
-                                                if (ep >= &expbuf[ESIZE])
-                                                        goto cerror;
-                                        }
-                                }
-                                *ep++ = c;
-                                cclcnt++;
-                                if (ep >= &expbuf[ESIZE])
-                                        goto cerror;
-                        } while ((c = getchr()) != ']');
-                        lastep[1] = cclcnt;
+                        lastep[1] = compile_bracket(&s, eof);
+                        if (lastep[1] < 0)
+                                goto cerror;
                         continue;
 
                 defchar:
                 default:
-                        *ep++ = CCHR;
-                        *ep++ = c;
+                        buffer_putc(&expbuf, CCHR);
+                        buffer_putc(&expbuf, c);
                 }
         }
    cerror:
-        expbuf[0] = '\0';
+        free(tbuf);
+        expbuf.base[0] = '\0';
         nbra = 0;
         qerror();
 }
